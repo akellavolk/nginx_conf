7612f325d765f035b5ce282dfc96044abbe0fac6
akellavolk <feanor28@yandex.ru>
2017-10-11 13:05:40 +0300

    Add -f key at ln

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
