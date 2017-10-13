7232f229358a26e3a5ae9e790015f4de40f021d9
akellavolk <feanor28@yandex.ru>
2017-10-13 19:07:31 +0300

    Add local.conf

---
+++ etc/hello.py
@@ -0,0 +1,10 @@
+def app(envirinment, start_response):
+    """Simplest possible application object"""
+    data = 'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
