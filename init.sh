116dce66014c847861a1c3cfcda9e6f688a27630
Rostislav <feanor28@yandex.ru>
2017-10-14 16:43:51 +0300

    Fix stepik path

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
 
-#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-#sudo gunicorn -c /home/box/web/hello.py hello:app
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo gunicorn -c /home/box/web/etc/hello.py hello:app
 
 sudo ln -sf /home/akellavolk/dev/stepik/web_mailru/server/etc/local.conf /etc/nginx/external.conf
 sudo /etc/init.d/nginx restart
