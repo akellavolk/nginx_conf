83017e1fa803de1734851875e9f63a298f3667a8
Rostislav <feanor28@yandex.ru>
2017-10-14 16:26:39 +0300

    Fix return to byte

--- hello.py
+++ hello.py
@@ -10,4 +10,4 @@ def app(environ, start_response):
         ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return iter(body)
+    return [byte(body, 'utf-8')]
