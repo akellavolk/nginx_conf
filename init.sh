254bd81c10de571727c8eabfb23aab8092c8de3d
Rostislav <feanor28@yandex.ru>
2017-10-14 15:53:30 +0300

    Add start unicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
+
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#sudo gunicorn -c /home/box/web/hello.py hello:app
+
+sudo ln -sf /home/akellavolk/dev/stepik/web_mailru/server/etc/local.conf /etc/nginx/external.conf
 sudo /etc/init.d/nginx restart
+
+sudo gunicorn -c /home/akellavolk/dev/stepik/web_mailru/server/etc/hello.py hello:app
