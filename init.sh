62ea744404c29ec773c3df5538d19783c562b3d9
Rostislav <feanor28@yandex.ru>
2017-10-14 17:31:01 +0300

    Delete test row

--- init.sh
+++ init.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo gunicorn -c /home/box/web/etc/hello.py hello:app
-
-sudo ln -sf /home/akellavolk/dev/stepik/web_mailru/server/etc/local.conf /etc/nginx/external.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -c /home/akellavolk/dev/stepik/web_mailru/server/etc/hello.py hello:app
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
