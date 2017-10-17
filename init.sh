d7f3a1aaaddaa18de478bf360e862116ad87dcb2
Rostislav <feanor28@yandex.ru>
2017-10-17 20:39:16 +0300

    add one more app to gunicorn

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+# Gunicorn
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -c /etc/gunicorn.d/hello.py ask.wsgi:application
+
